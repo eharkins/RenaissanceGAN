45847c3bff349490f3a2ba321cab9da7ef023793
Aidan <a_hendrickson@coloradocollege.edu>
2018-02-11 23:57:29 -0700

    leaky relu gan--this generates good eyes

--- from_scratch_mnist_gan/gan_model.py
+++ from_scratch_mnist_gan/gan_model.py
@@ -23,16 +23,19 @@ def makeGenerator(data_shape, noise_vect_size):
     generator.add(Dense(data_size, activation = 'sigmoid', input_dim=noise_vect_size, kernel_initializer=initializers.RandomNormal(stddev=0.02)))
     #could random normal be responsible for lack of color variation?
     #generator.add(Dropout(.1))
-    #generator.add(Dense(data_size, activation = 'sigmoid'))
+    generator.add(Dense(data_size, activation = 'sigmoid'))
     #generator.add(Dense(data_size, activation = 'sigmoid', input_dim=noise_vect_size, kernel_initializer=initializers.RandomNormal(stddev=0.02)))
     #generator.add(Dropout(.1))
 
     # generator.add(Dense(data_size, activation = 'sigmoid'))
     #generator.add(Dropout(.1))
     generator.add(Reshape((data_shape), input_shape=(data_size,)))
-    generator.add(Conv2DTranspose(channels, (3, 12), padding='same', activation = 'sigmoid'))
-    generator.add(Conv2DTranspose(channels, (3, 12), strides = (1,1), padding='same', activation = 'sigmoid'))
-    
+    generator.add(Conv2DTranspose(32, (3, 3), padding='same'))
+    generator.add(LeakyReLU(0.1))
+    generator.add(Conv2DTranspose(channels, (3, 3), padding='same', activation = 'relu'))
+    #generator.add(LeakyReLU(0.1))
+    #generator.add(Conv2DTranspose(channels, (3, 3), strides = (1,1), padding='same', activation = 'sigmoid'))
+
     generator.compile(loss = 'binary_crossentropy', optimizer = adam)
     return generator
 
@@ -40,12 +43,12 @@ def makeDiscriminator(data_shape):
     #create discriminator
     data_size = data_shape[0]*data_shape[1]*data_shape[2]
     discriminator = Sequential()
-    # discriminator.add(Conv2D(64, (3, 3), padding='same', input_shape=(data_shape), activation = 'sigmoid'))
-    # discriminator.add(MaxPooling2D(pool_size=(2, 2))) #max pooling is very important! without it, the GAN takes longer and produces only noise
-    # discriminator.add(Conv2D(64, (3, 3), padding='same', input_shape=(data_shape), activation = 'sigmoid'))
-    # discriminator.add(MaxPooling2D(pool_size=(2, 2)))
-    discriminator.add(Conv2D(64, (3, 12), padding='same', input_shape=(data_shape), activation = 'sigmoid'))
-    # discriminator.add(MaxPooling2D(pool_size=(2, 2)))
+    discriminator.add(Conv2D(64, (3, 3), padding='same', input_shape=(data_shape), activation = 'relu'))
+    discriminator.add(MaxPooling2D(pool_size=(2, 2))) #max pooling is very important! without it, the GAN takes longer and produces only noise
+    discriminator.add(Conv2D(64, (3, 3), input_shape=(data_shape), padding='same', activation = 'relu'))
+    discriminator.add(MaxPooling2D(pool_size=(2, 2)))
+    discriminator.add(Conv2D(64, (3, 3), input_shape=(data_shape), padding='same', activation = 'relu'))
+    discriminator.add(MaxPooling2D(pool_size=(2, 2)))
     discriminator.add(Flatten())
     discriminator.add(Dense(32, activation = 'sigmoid', input_dim=data_size, kernel_initializer=initializers.RandomNormal(stddev=0.02)))
     discriminator.add(Dense(1, activation = 'sigmoid'))
